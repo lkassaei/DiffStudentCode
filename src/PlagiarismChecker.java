cf186b5071392c03eeeb8c792c1900ed6e2f91de
lkassaei <lily.kassaei@menloschool.org>
2026-02-08 14:00:31 -0800

    v1

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -3,7 +3,7 @@
  * A tool for finding the longest shared substring between two documents.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Lily Kassaei
  */
 public class PlagiarismChecker {
 
@@ -17,7 +17,33 @@ public class PlagiarismChecker {
     public static int longestSharedSubstring(String doc1, String doc2) {
 
         // TODO Complete this function to return the length of the longest shared substring.
+        int count = 0;
+        int len = 0;
+        String str1 = "";
+        String str2 = "";
+        if (doc1.length() > doc2.length()) {
+            len = doc2.length();
+            str1 = doc2;
+            str2 = doc1;
+        }
+        else {
+            len = doc1.length();
+            str1 = doc1;
+            str2 = doc2;
+        }
+        int index = 1;
+        for (int i = 0; i < len; i++) {
+            if (i + index < len) {
+                String curr = str1.substring(i, i + index);
+                if (str2.contains(curr)) {
+                    index++;
+                }
+                else {
+                    index = 0;
+                }
+            }
 
-        return 0;
+        }
+        return index;
     }
 }
